069f0278e9489fea43e1750105ee7d6030a085c5
zixun <lvzixun@gmail.com>
2014-09-18 21:51:37 +0800

    del read / write function at base class

--- src/SprotoTypeBase.cs
+++ src/SprotoTypeBase.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 namespace Sproto {
 	public class SprotoTypeBase {
 		protected SprotoTypeFieldOP has_field;
-		private SprotoTypeSerialize serialize;
+		protected SprotoTypeSerialize serialize;
+		protected SprotoTypeDeserialize deserialize;
 
 
 		public SprotoTypeBase(int max_field_count) {
@@ -14,72 +15,34 @@ namespace Sproto {
 		}
 
 
-		// write integer
-		protected void write_integer(Int64 integer, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_integer (integer, tag);
+		public SprotoTypeBase(int max_field_count, byte[] buffer) {
+			this.has_field = new SprotoTypeFieldOP (max_field_count);
+			this.deserialize = new SprotoTypeDeserialize (buffer);
 		}
 
-		protected void write_intger(List<Int64> integer_list, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_integer (integer_list, tag);
+		public virtual byte[] encode () {
+			throw new Exception ("no encode function.");
 		}
 
-
-		// write string
-		protected void write_string(string str, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_string (str, tag);
-		}
-
-		protected void write_string(List<string> str_list, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_string (str_list, tag);
-		}
-
-
-		// write boolean
-		protected void write_boolean(bool b, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_boolean (b, tag);
-		}
-
-		protected void write_boolean(List<bool> b_list, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_boolean (b_list, tag);
-		}
-
-		// write struct
-		protected void write_obj(SprotoTypeBase obj, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_obj (obj, tag);
-		}
-
-		protected void srite_obj(List<SprotoTypeBase> obj_list, int tag, int field_idx) {
-			this.has_field.set_field (field_idx, true);
-			this.serialize.write_obj (obj_list, tag);
-		}
-
-
-		public byte[] encode() {
+		public byte[] pack(byte[] buffer) {
 			return null;
 		}
 
-		public void pack(){
-
-		}
-
-		public void unpack(){
-
+		public byte[] unpack(byte[] buffer) {
+			return null;
 		}
 
 		public void clear(){
-
 			// clear has slot
 			this.has_field.clear_field ();
 
 			// clear serialize
-			this.serialize.clear ();
+			if(this.serialize != null)
+				this.serialize.clear ();
+
+			// clear deserialize
+			if (this.deserialize != null)
+				this.deserialize.clear ();
 		}
 	}
 
