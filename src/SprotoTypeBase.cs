abb4543fbc3e55b5a2241100aa3f87746857c893
zixun <lvzixun@gmail.com>
2014-09-21 12:21:21 +0800

    add unpack pack to SprotoTypeBase

--- src/SprotoTypeBase.cs
+++ src/SprotoTypeBase.cs
@@ -7,11 +7,13 @@ namespace Sproto {
 		protected SprotoTypeFieldOP has_field;
 		protected SprotoTypeSerialize serialize;
 		protected SprotoTypeDeserialize deserialize;
+		protected SprotoTypeExtract extract;
 
 
 		public SprotoTypeBase(int max_field_count) {
 			this.has_field = new SprotoTypeFieldOP (max_field_count);
 			this.serialize = new SprotoTypeSerialize (max_field_count);
+			this.extract = new SprotoTypeExtract ();
 		}
 
 		public virtual void init (byte[] buffer){
@@ -22,17 +24,18 @@ namespace Sproto {
 		public SprotoTypeBase(int max_field_count, byte[] buffer) {
 			this.has_field = new SprotoTypeFieldOP (max_field_count);
 			this.deserialize = new SprotoTypeDeserialize (buffer);
+			this.extract = new SprotoTypeExtract ();
 		}
 
 		public abstract byte[] encode ();
 		protected abstract void decode (byte[] buffer);
 
 		public byte[] pack(byte[] buffer) {
-			return null;
+			return this.extract.pack (buffer);
 		}
 
 		public byte[] unpack(byte[] buffer) {
-			return null;
+			return this.extract.unpack (buffer);
 		}
 
 		public void clear(){
